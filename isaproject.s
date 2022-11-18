fecc31066e04b18dbb9751386dfbe607d0e2ecce
jaywynes <jayden.wynes@gmail.com>
2022-11-17 22:08:25 -0500

    commit for test run of main

--- isaproject.s
+++ isaproject.s
@@ -290,50 +290,6 @@ blr printf
 
 // sort(ia)
 
-// printf("Something bad");
-// Kernel call to printf expects parameters
-// r1 - address of format string - "Something bad"
-// mva r1, bad
-// ker #0x11
-// The os has code for printf at address 0x7000
-// The code there generates the ker instruction
-// You call printf with
-// r0 - has address of format string - "Something bad"
-
-//
-// for (int i = 0; i < 4; i++)
-//     printf("ia[%d]: %d", i, sia[i]);
-mov r4, 0          // i to r4
-mva r5, sia   // address is sia to r5
-.label loop4times  // print 3 elements if sia
-cmp r4, 4
-bge end_loop4times
-// Kernel call to printf expects parameters
-// r1 - address of format string - "ia[%d]: %d"
-// r2 - value for first %d
-// r3 - value for second %d
-mva r1, fmt1       // fmt1 to r1
-mov r2, r4         // i to r2
-ldr r3, [r5], 4    // sia[i] to r3
-ker #0x11          // Kernel call to printf
-adi r4, r4, 1      // i++
-bal loop4times
-.label end_loop4times
-// int n = numelems(sia);
-mva r0, sia        // put address of sia in r0
-blr numelems       // n = numelems(sia)
-str r0, [sp, 4]
-// int sm1 = smallest(sia);
-mva r0, sia        // put address of sia in r0
-blr smallest       // sm1 = smallest(sia)
-str r0, [sp, 8]    // store return value in sm1
-
-
-
-// Do not deallocate stack.
-// This leaves r13 with an address that can be used to dump memory
-// > d 0x4ff0 
-// Shows the three hardcoded values stored in cav, n, and sm1.
-// 0x4ff0 (0d20464) 0xffffffff 0x0000000a 0x00000002
 .label end
-bal end            // branch to self
+bal end
+
